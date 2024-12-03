de4adbd48c557ad86aed9dfcc59d5cdd4287bb66
Tom-BUXDAO <Buxdao@gmail.com>
2024-12-03 20:03:49 +0000

    Fix Prisma client initialization and auth config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,23 @@
 import { PrismaClient } from '@prisma/client'
 
-// Add type declaration for global prisma instance
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+// Learn more: https://pris.ly/d/help/next-js-best-practices
+
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
 
-export const prisma = global.prisma || new PrismaClient()
+let prisma: PrismaClient
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
 }
 
-export default prisma
+export { prisma }
